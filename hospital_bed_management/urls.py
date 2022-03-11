5065b347bd11b0587584cbc706bb78772cc90be9
ethan-cn <2612869d@student.gla.ac.uk>
2022-03-11 15:53:32 +0000

    edit

--- hospital_bed_management/urls.py
+++ hospital_bed_management/urls.py
@@ -61,4 +61,6 @@ urlpatterns = [
                   path('get_department_by_id/', patient_views.get_department_by_id),
                   path('admin/', admin.site.urls),
 
+
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
